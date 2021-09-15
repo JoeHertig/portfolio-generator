15cc4f2130d07e7fcfdd546ed27de46ebad97dda
Joe Hertig <joehertig94@gmail.com>
2021-09-15 16:13:12 -0600

    added name feature

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
-const fs = require("fs");
-const generatePage = require("./src/page-template.js");
-const profileDataArgs = process.argv.slice(2);
-const [name, github] = profileDataArgs;
+const inquirer = require("inquirer");
+// const fs = require("fs");
+// const generatePage = require("./src/page-template");
 
-fs.writeFile("./index.html", generatePage(name, github), (err) => {
-  if (err) throw err;
+// // const profileDataArgs = process.argv.slice(2);
 
-  console.log("Portfolio complete! Check out index.html to see the output!");
-});
+// // const [name, github] = profileDataArgs;
 
-// const printProfileData = (profileDataArr) => {
-//   // This...
-//   for (let i = 0; i < profileDataArr.length; i += 1) {
-//     console.log(profileDataArr[i]);
-//   }
+// const pageHTML = generatePage(name, github);
 
-//   console.log("================");
+// fs.writeFile("./index.html", pageHTML, (err) => {
+//   if (err) throw err;
 
-//   // Is the same as this...
-//   profileDataArr.forEach((profileItem) => console.log(profileItem));
-// };
+//   console.log("Portfolio complete! Check out index.html to see the output!");
+// });
 
-// printProfileData(profileDataArgs);
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is your name?",
+    },
+  ])
+  .then((answers) => console.log(answers));
